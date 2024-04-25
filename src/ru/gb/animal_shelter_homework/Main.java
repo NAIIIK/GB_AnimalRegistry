eaf01b72286316c84ad88d68d6b02a2652a63124
RodionovNikita <radzivonaumikita@gmail.com>
2024-04-25 23:33:30 +0200

    fixed Main class

--- src/ru/gb/animal_shelter_homework/Main.java
+++ src/ru/gb/animal_shelter_homework/Main.java
@@ -2,17 +2,10 @@ package ru.gb.animal_shelter_homework;
 
 import ru.gb.animal_shelter_homework.UI.ConsoleUI;
 import ru.gb.animal_shelter_homework.UI.View;
-import ru.gb.animal_shelter_homework.animal_shelter_API.animals.Animal;
-import ru.gb.animal_shelter_homework.animal_shelter_API.animals.pets.Cat;
-
-import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-//        View view = new ConsoleUI();
-//        view.start();
-//        LocalDate date = LocalDate.of(2001, 7, 15);
-//        Animal animal = new Cat("marcel", date);
-//        System.out.println(animal);
+        View view = new ConsoleUI();
+        view.start();
     }
 }
